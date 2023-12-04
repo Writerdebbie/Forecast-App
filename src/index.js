317b9f2a17ffa48ff940ee2700b9b605f8dea623
gloria <nnamdidebbie7@gmail.com>
2023-12-03 18:15:08 -0800

    Injected the Forecast data from Javascript

--- src/index.js
+++ src/index.js
@@ -52,6 +52,30 @@ function initiateSearch(event) {
     
     searchCity (searchInput.value)
 }
+
+function showForecast() {
+    
+
+    let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHTML = "";
+
+    days.forEach(function (day) {
+         forecastHTML = forecastHTML + `<div class="forecast-day">
+            <div class="forecast-date">${day}</div>
+            <div class="forecast-icon">⛅</div>
+            <div class="forecast-temperature">
+              <div class="forecast-temperature"><strong>15°</strong></div>
+              <div class="forecast-temperature">10°</div>
+            </div>
+          </div>
+          `;
+
+    });
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHTML;
+ }  
+
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", initiateSearch);
-searchCity("Paris")
+searchCity("Paris");
+showForecast();
